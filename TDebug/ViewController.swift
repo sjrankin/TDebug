5785eaa985b1b10a322941c34700aa233e9c2a22
Stuart Rankin <sjrankin@gmail.com>
2019-03-25 12:16:22 +0900

    Initial Commit

---
+++ TDebug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDebug
+//
+//  Created by Stuart Rankin on 3/25/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
