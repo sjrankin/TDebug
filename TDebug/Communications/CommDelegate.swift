26c73b98e1e4ba3f628fd8b32408cfcc1fbe31c2
Stuart Rankin <sjrankin@gmail.com>
2019-03-27 22:48:36 +0900

    Create CommDelegate.swift
    Moved to new directory.

---
+++ TDebug/Communications/CommDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  CommDelegate.swift
+//  TDebug
+//
+//  Created by Stuart Rankin on 3/26/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol CommDelegate: class
+{
+    func RawDataReceived(_ RawData: String, _ BytesRead: Int)
+    func RemoteServerList( _ List: [String])
+}
