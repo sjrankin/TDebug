23f403399aaef64871c3289013e035ca549dcdc9
Stuart Rankin <sjrankin@gmail.com>
2019-03-27 22:49:33 +0900

    Create LogItemProtocol.swift
    Protocol to set a log item to be viewed.

---
+++ TDebug/LogDisplay/LogItemProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  LogItemProtocol.swift
+//  TDebug
+//
+//  Created by Stuart Rankin on 3/27/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol LogItemProtocol: class
+{
+    func SetLogItem(_ Item: LogItem)
+}
