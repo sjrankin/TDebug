745418ab78c50fce9d56b462baa02bb679593414
Stuart Rankin <sjrankin@gmail.com>
2019-03-29 22:00:53 +0900

    Create MainProtocol.swift
    New protocol to expose some functionality in the main UI code to other classes.

---
+++ TDebug/MainProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MainProtocol.swift
+//  TDebug
+//
+//  Created by Stuart Rankin on 3/29/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol MainProtocol: class
+{
+    var MPManager: MultiPeerManager {get}
+}
