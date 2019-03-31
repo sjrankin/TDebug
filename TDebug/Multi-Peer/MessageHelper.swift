252405cb47c4e9b9eb84abac3ee9ea28123bc229
Stuart Rankin <sjrankin@gmail.com>
2019-03-31 22:03:04 +0900

    Update MessageHelper.swift
    Added multiple encoding and decoding routines. Added special command handling. Add AssembleCommand.

--- TDebug/Multi-Peer/MessageHelper.swift
+++ TDebug/Multi-Peer/MessageHelper.swift
@@ -21,17 +21,212 @@ class MessageHelper
         let Parts = Raw.split(separator: String.Element(Delimiter))
         if Parts.count != 2
         {
-            print("Split into incorrect number of parts: \(Parts.count), expected 2.")
+            //print("Split into incorrect number of parts: \(Parts.count), expected 2. Raw=\"\(Raw)\"")
             return nil
         }
         return (String(Parts[0]), String(Parts[1]))
     }
     
-    public static func DecodeMessage(_ Raw: String) -> (MessageTypes, String, String, String)
+    public static func DecodeSpecialCommand(_ Raw: String) -> SpecialCommands
+    {
+        let Delimiter = String(Raw.first!)
+        var Next = Raw
+        Next.removeFirst()
+        let Parts = Next.split(separator: String.Element(Delimiter))
+        if Parts.count != 2
+        {
+            return SpecialCommands.Unknown
+        }
+        let SCmd = String(Parts[1])
+        for (Command, Indicator) in SpecialCommmandIndicators
+        {
+            if Indicator.lowercased() == SCmd.lowercased()
+            {
+                return Command
+            }
+        }
+        return SpecialCommands.Unknown
+    }
+    
+    public static func DecodeKVPMessage(_ Raw: String) -> (UUID?, String, String)
+    {
+        let Delimiter = String(Raw.first!)
+        var Next = Raw
+        Next.removeFirst()
+        let Parts = Next.split(separator: String.Element(Delimiter))
+        var PartsList = [(String, String)]()
+        for Part in Parts
+        {
+            if let (Key, Value) = DecodeKVP(String(Part), Delimiter: "=")
+            {
+                PartsList.append((Key, Value))
+            }
+        }
+        var IDs = ""
+        var KeyValue = ""
+        var ValueValue = ""
+        for (Key, Value) in PartsList
+        {
+            switch Key
+            {
+            case "ID":
+                IDs = Value
+                
+            case "Key":
+                KeyValue = Value
+                
+            case "Value":
+                ValueValue = Value
+                
+            default:
+                break
+            }
+        }
+        if let FinalID = UUID(uuidString: IDs)
+        {
+            return (FinalID, KeyValue, ValueValue)
+        }
+        return (nil, KeyValue, ValueValue)
+    }
+    
+    public static func DecodeEchoMessage(_ Raw: String) -> (String, String, Int, Int)
+    {
+        //[Command, ReturnAddress, EchoMessage, EchoDelay, EchoCount]
+        let Delimiter = String(Raw.first!)
+        var Next = Raw
+        Next.removeFirst()
+        let Parts = Next.split(separator: String.Element(Delimiter))
+        var PartsList = [(String, String)]()
+        for Part in Parts
+        {
+            if let (Key, Value) = DecodeKVP(String(Part), Delimiter: "=")
+            {
+                PartsList.append((Key, Value))
+            }
+        }
+        var Message = ""
+        var EchoTo = ""
+        var Delay = 1
+        var Count = 1
+        for (Key, Value) in PartsList
+        {
+            switch Key
+            {
+            case "Delay":
+                if let D = Int(Value)
+                {
+                    Delay = D
+                }
+                
+            case "Count":
+                if let C = Int(Value)
+                {
+                    Count = C
+                }
+                
+            case "Message":
+                Message = Value
+                
+            case "EchoBackTo":
+                EchoTo = Value
+                
+            default:
+                break
+            }
+        }
+        return (Message, EchoTo, Delay, Count)
+    }
+    
+    //Format of command: command,address{,data}
+    //returns command, address, text, fg color, bg color
+    public static func DecodeIdiotLightMessage(_ Raw: String) ->(IdiotLightCommands, String, String?, UIColor?, UIColor?)
+    {
+        let Delimiter = String(Raw.first!)
+        var Next = Raw
+        Next.removeFirst()
+        let Parts = Next.split(separator: String.Element(Delimiter))
+        
+        if Parts.count < 2
+        {
+            return (.Unknown, "", nil, nil, nil)
+        }
+        var PartsList = [(String, String)]()
+        for Part in Parts
+        {
+            if let (Key, Value) = DecodeKVP(String(Part), Delimiter: "=")
+            {
+                PartsList.append((Key, Value))
+            }
+        }
+        var Address = ""
+        var Text: String? = nil
+        var Command: IdiotLightCommands = .Unknown
+        var BGColor: UIColor? = nil
+        var FGColor: UIColor? = nil
+        for Part in PartsList
+        {
+            switch Part.0
+            {
+            case "Address":
+                Address = Part.1
+                
+            case "Enable":
+                if Part.1.lowercased() == "yes"
+                {
+                    Command = .Enable
+                }
+                else
+                {
+                    Command = .Disable
+                }
+                break
+                
+            case "Text":
+                Command = .SetText
+                Text = Part.1
+                
+            case "BGColor":
+                Command = .SetBGColor
+                BGColor = UIColor(HexString: Part.1)!
+                
+            case "FGColor":
+                Command = .SetFGColor
+                FGColor = UIColor(HexString: Part.1)!
+                
+            default:
+                continue
+            }
+        }
+        return (Command, Address, Text, FGColor, BGColor)
+    }
+    
+    public static func GetMessageType(_ Raw: String) -> MessageTypes
     {
         if Raw.isEmpty
         {
-            return (MessageTypes.Unknown, "", "", "")
+            return MessageTypes.Unknown
+        }
+        let Delimiter = String(Raw.first!)
+        var Next = Raw
+        Next.removeFirst()
+        let Parts = Next.split(separator: String.Element(Delimiter))
+        for Part in Parts
+        {
+            if Part.isEmpty
+            {
+                continue
+            }
+            let MessageType = MessageTypeFromID(String(Part))
+            return MessageType
+        }
+        return .Unknown
+    }
+    
+    public static func DecodeMessageEx(_ Raw: String) -> (String, MessageTypes, String, String, String)
+    {
+        if Raw.isEmpty
+        {
+            return ("", MessageTypes.Unknown, "", "", "")
         }
         let Delimiter = String(Raw.first!)
         var Next = Raw
@@ -40,9 +235,15 @@ class MessageHelper
         if Parts.count != 4
         {
             //Assume the last item in the parts list is the message and return it as an unknown type.
-            return (MessageTypes.Unknown, "", "", String(Parts[Parts.count - 1]))
+            return (Delimiter, MessageTypes.Unknown, "", "", String(Parts[Parts.count - 1]))
         }
-        return (MessageTypeFromID(String(Parts[0])), String(Parts[1]), String(Parts[2]), String(Parts[3]))
+        return (Delimiter, MessageTypeFromID(String(Parts[0])), String(Parts[1]), String(Parts[2]), String(Parts[3]))
+    }
+    
+    public static func DecodeMessage(_ Raw: String) -> (MessageTypes, String, String, String)
+    {
+        let (_, MType, P1, P2, P3) = DecodeMessageEx(Raw)
+        return (MType, P1, P2, P3)
     }
     
     public static func DecodeHeartbeat(_ Raw: String) -> (Int, String?)?
@@ -114,12 +315,6 @@ class MessageHelper
             return (MessageTypes.Unknown, "", "", String(Parts[Parts.count - 1]))
         }
         return (MessageTypeFromID(String(Parts[0])), String(Parts[1]), String(Parts[2]), String(Parts[3]))
-    }
-    
-    public static func GetMessageType(_ Raw: String) -> MessageTypes
-    {
-        let (MType, _, _, _) = DecodeMessage(Raw)
-        return MType
     }
     
     public static func MessageTypeFromID(_ RawID: String) -> MessageTypes
@@ -226,50 +421,91 @@ class MessageHelper
     public static func MakeHeartbeatMessage(Payload: String, NextExpectedIn: Int, _ HostName: String) -> String
     {
         var Message = "Next=\(NextExpectedIn)"
-        let Delimiter = GetUnusedDelimiter(From: ["="])
-        Message = Delimiter + Message + Delimiter + "Payload=" + Payload
+        let MPayload = "Payload=\(Payload)"
+        let Delimiter = GetUnusedDelimiter(From: [Message, MPayload])
+        Message = Delimiter + Message + Delimiter + MPayload
         return MakeMessage(WithType: .Heartbeat, Message, HostName)
     }
     
     public static func MakeIdiotLightMessage(Address: String, State: UIFeatureStates) -> String
-{
-    let Command = MessageTypeIndicators[.ControlIdiotLight]!
-    let Action = "Enable=" + [UIFeatureStates.Disabled: "No", UIFeatureStates.Enabled: "Yes"][State]!
-    let Delimiter = GetUnusedDelimiter(From: [Command, Action])
-    let Final = Delimiter + Command + Delimiter + Action
-    return Final
+    {
+        let Command = MessageTypeIndicators[.ControlIdiotLight]!
+        let Addr = "Address=\(Address)"
+        let Action = "Enable=" + [UIFeatureStates.Disabled: "No", UIFeatureStates.Enabled: "Yes"][State]!
+        let Delimiter = GetUnusedDelimiter(From: [Command, Addr, Action])
+        let Final = Delimiter + Command + Delimiter + Addr + Delimiter + Action
+        return Final
     }
     
     public static func MakeIdiotLightMessage(Address: String, Text: String) -> String
     {
         let Command = MessageTypeIndicators[.ControlIdiotLight]!
+        let Addr = "Address=\(Address)"
         let Action = "Text=" + Text
-        let Delimiter = GetUnusedDelimiter(From: [Command, Action])
-        let Final = Delimiter + Command + Delimiter + Action
+        let Delimiter = GetUnusedDelimiter(From: [Command, Addr, Action])
+        let Final = Delimiter + Command + Delimiter + Addr + Delimiter + Action
         return Final
     }
     
-    public static func MakeIdiotLightMessage(Address: String, FGColor: UIColor? = nil, BGColor: UIColor? = nil) -> String
+    public static func MakeIdiotLightMessage(Address: String, FGColor: UIColor) -> String
     {
-        if FGColor == nil && BGColor == nil
-        {
-            return ""
-        }
         let Command = MessageTypeIndicators[.ControlIdiotLight]!
-        var FG = "nil"
-        if let FGC = FGColor
+        let Addr = "Address=\(Address)"
+        let Action1 = "FGColor=" + FGColor.AsHexString()
+        let Delimiter = GetUnusedDelimiter(From: [Command, Addr, Action1])
+        let Final = Delimiter + Command + Delimiter + Addr + Delimiter + Action1
+        return Final
+    }
+    
+    public static func MakeIdiotLightMessage(Address: String, BGColor: UIColor) -> String
+    {
+        let Command = MessageTypeIndicators[.ControlIdiotLight]!
+        let Addr = "Address=\(Address)"
+        let Action1 = "BGColor=" + BGColor.AsHexString()
+        let Delimiter = GetUnusedDelimiter(From: [Command, Addr, Action1])
+        let Final = Delimiter + Command + Delimiter + Addr + Delimiter + Action1
+        return Final
+    }
+    
+    public static func MakeEchoMessage(Message: String, Delay: Int, Count: Int, Host: String) -> String
+    {
+        let Command = MessageTypeIndicators[.EchoMessage]!
+        let ReturnAddress = "EchoBackTo=\(Host)"
+        let EchoCount = "Count=\(Count)"
+        let EchoDelay = "Delay=\(Delay)"
+        let EchoMessage = "Message=\(Message)"
+        let Delimiter = GetUnusedDelimiter(From: [Command, ReturnAddress, EchoCount, EchoDelay, EchoMessage])
+        let Final = AssembleCommand(FromParts: [Command, ReturnAddress, EchoMessage, EchoDelay, EchoCount], WithDelimiter: Delimiter)
+    return Final
+    }
+    
+    public static func MakeKVPMessage(ID: UUID, Key: String, Value: String) -> String
+    {
+        let Command = MessageTypeIndicators[.KVPData]!
+        let IDCmd = "ID=\(ID.uuidString)"
+        let KeyString = "Key=\(Key)"
+        let ValueString = "Value=\(Value)"
+        let Delimiter = GetUnusedDelimiter(From: [Command, IDCmd, KeyString, ValueString])
+        let Final = AssembleCommand(FromParts: [Command, IDCmd, KeyString, ValueString], WithDelimiter: Delimiter)
+        return Final
+    }
+    
+    public static func MakeSpcialCommand(_ Command: SpecialCommands) -> String
+    {
+        let Cmd = MessageTypeIndicators[.SpecialCommand]!
+        let SCmd = SpecialCommmandIndicators[Command]!
+        let Delimiter = GetUnusedDelimiter(From: [Cmd, SCmd])
+        let Final = AssembleCommand(FromParts: [Cmd, SCmd], WithDelimiter: Delimiter)
+        return Final
+    }
+    
+    static func AssembleCommand(FromParts: [String], WithDelimiter: String) -> String
+    {
+        var Final = ""
+        for Part in FromParts
         {
-            FG = FGC.AsHexString()
+            Final = Final + WithDelimiter + Part
         }
-        var BG = "nil"
-        if let BGC = BGColor
-        {
-            BG = BGC.AsHexString()
-        }
-        let Action1 = "FGColor=" + FG
-        let Action2 = "BGColor=" + BG
-        let Delimiter = GetUnusedDelimiter(From: [Command, Action1, Action2])
-        let Final = Delimiter + Command + Delimiter + Action1 + Delimiter + Action2
         return Final
     }
     
@@ -281,10 +517,27 @@ class MessageHelper
             MessageTypes.EchoMessage: "9a904bd0-117b-4548-b31f-da2b4c3807dd",
             MessageTypes.Acknowledge: "73783e04-cad4-42a4-a3b3-449efcabf592",
             MessageTypes.Heartbeat: "5d8a38fd-878a-458f-aa80-62d810e520c1",
-            MessageTypes.StatusValue: "280b67c2-c7bc-4fcb-b287-41d2539f4680",
-            MessageTypes.IDValue: "4c2805b8-d5ad-4c68-a5f8-1f554a90671a",
+            MessageTypes.KVPData: "4c2805b8-d5ad-4c68-a5f8-1f554a90671a",
+            MessageTypes.EchoReturn: "970bac64-f399-499d-8db6-c65e508ae40d",
+            MessageTypes.SpecialCommand: "e83a5588-b285-49ee-b2fe-95f803f073b7",
             MessageTypes.Unknown: "dfc5b2d5-521b-46a8-b459-a4947089312c",
     ]
+    
+    private static let SpecialCommmandIndicators: [SpecialCommands: String] =
+    [
+        .ClearKVPList: "a1a4974c-ed8f-41bc-bdbf-49570f67cc03",
+        .ClearLogList: "283c06c3-dca6-4044-a8ba-b034efd51594",
+        .ClearIdiotLights: "1600bf5d-ffa7-474b-ab55-c8298f056969",
+        .Unknown: "bbfb4205-d9f6-49cf-bd96-630641d4fb16",
+    ]
+}
+
+enum SpecialCommands: Int
+{
+    case ClearKVPList = 0
+    case ClearLogList = 1
+    case ClearIdiotLights = 2
+    case Unknown = 10000
 }
 
 enum MessageTypes: Int
@@ -295,8 +548,9 @@ enum MessageTypes: Int
     case EchoMessage = 3
     case Acknowledge = 4
     case Heartbeat = 5
-    case StatusValue = 6
-    case IDValue = 7
+    case KVPData = 6
+    case EchoReturn = 7
+    case SpecialCommand = 8
     case Unknown = 10000
 }
 
@@ -304,4 +558,14 @@ enum UIFeatureStates: Int
 {
     case Disabled = 0
     case Enabled = 1
+}
+
+enum IdiotLightCommands: Int
+{
+    case Disable = 0
+    case Enable = 1
+    case SetText = 2
+    case SetFGColor = 3
+    case SetBGColor = 4
+    case Unknown = 10000
 }
