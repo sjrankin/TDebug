68cf71eb0b03ab4f2efc315a1f14ba280f858946
Stuart Rankin <sjrankin@gmail.com>
2019-04-02 20:12:03 +0900

    Update SendToRemoteCode.swift
    Changed a call to SendPreformatted to Send.

--- TDebug/SendTo/SendToRemoteCode.swift
+++ TDebug/SendTo/SendToRemoteCode.swift
@@ -69,7 +69,7 @@ class SendToRemoteCode: UIViewController, UITableViewDelegate, UITableViewDataSo
                 return
             }
             let PeerID = PeerList[SelectedIndex]
-            Main?.MPManager.SendPreformatted(Message: Message, To: PeerID)
+            Main?.MPManager.Send(Message: Message, To: PeerID)
         }
         TextBox.text = ""
     }
