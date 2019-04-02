b2933957fa2de46cf7af130e6c17e4d170726205
Stuart Rankin <sjrankin@gmail.com>
2019-04-02 22:13:47 +0900

    Update MultiPeerDelegate.swift
    Added OverrideMessageType and EncapsulatedID to ReceivedData.

--- TDebug/Multi-Peer/MultiPeerDelegate.swift
+++ TDebug/Multi-Peer/MultiPeerDelegate.swift
@@ -28,5 +28,21 @@ protocol MultiPeerDelegate
     ///   - Manager: The instance of the multipeer manager.
     ///   - Peer: The peer that sent the data. The data sent is not from a stream or a resource.
     ///   - RawData: The raw data in string format.
-    func ReceivedData(Manager: MultiPeerManager, Peer: MCPeerID, RawData: String)
+    ///   - OverrideMessageType: If present, this message type overrides the contents of the
+    ///                          message type in `RawData`.
+    ///   - EncapsulatedID: If present, the encapsulted ID value to use as a return value.
+    func ReceivedData(Manager: MultiPeerManager, Peer: MCPeerID, RawData: String,
+                      OverrideMessageType: MessageTypes?, EncapsulatedID: UUID?)
+    
+    /// Notifies the receiver of data received asynchronously as part of a request from the client.
+    ///
+    /// - Parameters:
+    ///   - Manager: The instance of the multipeer manager.
+    ///   - Peer: The peer that sent the data. The data sent is not from a stream or a resource.
+    ///   - CommandID: The ID of the command (but not the command ID). In other words, each encapsulated
+    ///                message is sent with a unique ID so that the responder can use the same ID when
+    ///                responding, allowing for synchronizing the response to the original request. This
+    ///                is the unique ID that allows for that.
+    ///   - RawData: The encapsulated message from the responder.
+    func ReceivedAsyncData(Manager: MultiPeerManager, Peer: MCPeerID, CommandID: UUID, RawData: String)
 }
