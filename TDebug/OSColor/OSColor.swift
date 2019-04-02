5b75a9de3e397c6404f0e7a5584570627508fefe
Stuart Rankin <sjrankin@gmail.com>
2019-04-02 22:09:24 +0900

    Create OSColor.swift
    Encapsulated NSColor and UIColor into OSColor.

---
+++ TDebug/OSColor/OSColor.swift
@@ -0,0 +1,28 @@
+//
+//  OSColor.swift
+//  TDebug
+//
+//  Created by Stuart Rankin on 4/2/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+#if FOR_MACOS
+import AppKit
+#else
+import UIKit
+#endif
+
+#if FOR_MACOS
+/// Ultra-thin wrapper around NSColor.
+class OSColor: NSColor
+{
+    
+}
+#else
+/// Ultra-thin wrapper around UIColor.
+class OSColor: UIColor
+{
+    
+}
+#endif
